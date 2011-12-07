20fad39f18862af9b2e81b651c71b7d23b02aca5
dominikcebula <dominikcebula@903e6750-0fa3-a6f8-dc88-28dfdd1b192a>
2011-12-07 18:34:48 +0000

    Switch to NetBeans

---
+++ dcstore-war/src/java/dcstore/web/CategoryWeb.java
@@ -0,0 +1,66 @@
+// This file is a part of dcstore project,
+// licensed under GPLv2
+//
+// Dominik Cebula
+// dominikcebula@gmail.com
+package dcstore.web;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.List;
+import dcstore.jpa.CategoryEntity;
+import dcstore.ejb.CategoryBean;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@RequestScoped
+public class CategoryWeb {
+
+    @EJB
+    private CategoryBean categoryBean;
+    private int id;
+    private String name;
+    private String nameNew;
+
+    public String getNameNew() {
+        return nameNew;
+    }
+
+    public void setNameNew(String nameNew) {
+        this.nameNew = nameNew;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public List<CategoryEntity> getAll() {
+        return categoryBean.getAll();
+    }
+    
+    public void add() throws Exception {
+        categoryBean.add(nameNew);
+    }
+
+    public void edit() throws Exception {
+        categoryBean.edit(id, name);
+        FacesContext.getCurrentInstance().getExternalContext().dispatch("index.xhtml?id=-1");
+    }
+    
+    public void del(int id) throws Exception {
+        categoryBean.del(id);
+    }
+}
